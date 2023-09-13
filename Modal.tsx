698ea0b1d1c7ae152c6640f97fffd6a1726c0ab4
rohanbhairi <84953343+rohanbhairi@users.noreply.github.com>
2023-09-13 23:44:04 +0530

    Add files via upload

---
+++ Modal.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import ModalContent, { Props as ModalContentProps } from "./ModalContent";
+
+interface Props extends ModalContentProps {
+  onModalClose: (e: React.MouseEvent<HTMLElement>) => void;
+  open: boolean;
+  rootElement: HTMLElement;
+}
+
+export default (props: Props) => {
+  if (!props.open) return null;
+
+  if (!props.rootElement) {
+    throw new Error('[react-calendly]: PopupModal rootElement property cannot be undefined')
+  }
+
+  return ReactDom.createPortal(
+    <div className="calendly-overlay">
+      <div
+        onClick={props.onModalClose}
+        className="calendly-close-overlay"
+      ></div>
+      <div className="calendly-popup">
+        <div className="calendly-popup-content">
+          <ModalContent {...props} />
+        </div>
+      </div>
+      <button
+        className="calendly-popup-close"
+        onClick={props.onModalClose}
+        aria-label="Close modal"
+        style={{
+          display: "block",
+          border: "none",
+          padding: 0,
+        }}
+      ></button>
+    </div>,
+    props.rootElement
+  );
+};
---
+++ ModalContent.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import "../../calendly-widget.css";
+import {
+  PageSettings,
+  Prefill,
+  Utm,
+  IframeTitle,
+  formatCalendlyUrl,
+} from "../../calendly";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+
+export interface Props {
+  url: string;
+  prefill?: Prefill;
+  utm?: Utm;
+  pageSettings?: PageSettings;
+  iframeTitle?: IframeTitle;
+}
+
+class ModalContent extends React.Component<Props, { isLoading: boolean }> {
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+    };
+
+    this.onLoad = this.onLoad.bind(this);
+  }
+
+  private onLoad() {
+    this.setState({
+      isLoading: false,
+    });
+  }
+
+  render() {
+    const src = formatCalendlyUrl({
+      url: this.props.url,
+      pageSettings: this.props.pageSettings,
+      prefill: this.props.prefill,
+      utm: this.props.utm,
+      embedType: "PopupWidget",
+    });
+
+    return (
+      <>
+        {this.state.isLoading && <LoadingSpinner />}
+        <iframe
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          title={this.props.iframeTitle || "Calendly Scheduling Page"}
+          onLoad={this.onLoad}
+          src={src}
+        ></iframe>
+      </>
+    );
+  }
+}
+
+export default ModalContent;
